893c8698805f5067ccbd8a3cff5ded1b061f60ba
cyyuanyuan <2500620013@qq.com>
2020-06-28 16:28:24 +0800

    严选

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//引入样式清空
+import './assets/Reset.css'
+//全局使用vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+//全局使用element-ui
+import Ele from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'; // 引入elementui样式库
+Vue.use(Ele)
+//全局使用axios
+import axios from 'axios'
+//将axios绑定到vue原型
+Vue.prototype.$axios=axios
+Vue.config.productionTip = false
+// 全局自定义指令
+Vue.directive("jump",(el,{ value },vnode,oldNode)=>{
+  el.onclick = ()=>{
+    // value
+    vnode.context.$router.push({
+        path:value
+    })
+  }
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<!-- 首页 -->
+<template>
+    <div>
+        <h3>我是首页</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    },
+
+    mounted(){
+        
+    },
+    methods: {
+        
+    },
+    components: {
+        
+    },
+}
+
+</script>
+<style scoped>
+    
+</style>
